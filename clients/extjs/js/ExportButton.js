3785e0db95c29264511de81f3b7ea9be23af1945
csmig <carlsmigielski@gmail.com>
2020-03-12 17:41:52 -0400

    Both APIs orchestrated

---
+++ clients/extjs/js/ExportButton.js
@@ -0,0 +1,231 @@
+/**
+ * $Id: ExportButton.js 807 2017-07-27 13:04:19Z csmig $
+ *
+ * @class Ext.ExportButton
+ * @extends Ext.Button
+ * A button for downloading local data, which optionally contains a menu.
+ * Original author: Carl Smigielski
+ * @constructor
+ * Creates a new ExportButton
+ */ 
+Ext.ux.ExportButton = Ext.extend(Ext.Button, {
+	constructor: function(config) {
+		
+		if (config.hasMenu == true) {
+			Ext.applyIf(config, {
+				menu: new Ext.menu.Menu({
+					items: [
+					{
+						text: "Grid data (.csv)"
+						,iconCls: 'sm-table-icon'
+						,exportType: 'grid'
+						,exportFormat: 'csv'
+						,href: "#"
+					}
+					,{
+						text: "Store data (.csv)"
+						,iconCls: 'sm-database-icon'
+						,exportType: 'store'
+						,exportFormat: 'csv'
+						,href: "#"
+					}]
+				}),
+			});
+		}
+		
+		this.gridBasename = (config.gridBasename || 'grid_export'),
+		this.gridSource = config.grid || undefined,
+		this.storeBasename = (config.storeBasename || 'store_export'),
+		this.storeSource = config.store || undefined,
+		this.exportType = config.exportType || 'grid';
+		this.exportFormat = config.exportFormat || 'csv';
+
+		Ext.ux.ExportButton.superclass.constructor.call(this, config);
+		
+		if (this.menu) {
+			this.menu.addListener('itemclick',this.menuItemClick,this);
+			this.menu.addListener('beforeshow',this.menuBeforeShow,this);
+		} else {
+			this.addListener('click',this.buttonClick,this);
+		}
+	},
+	
+    onRender: function(ct, position){
+		if (this.gridSource == undefined) {
+			this.gridSource = this.findParentByType('grid');
+		}
+		if (this.storeSource == undefined && this.gridSource != undefined) {
+			this.storeSource = this.gridSource.store;
+		}
+		Ext.ux.ExportButton.superclass.onRender.call(this,ct,position);
+	},
+	
+	buttonClick: function (btn, e) {
+		let csv = "\ufeff"; // UTF-8 BOM
+		let filename = "";
+		if (btn.exportFormat == 'csv'){
+			if (btn.exportType == 'grid') {
+				csv += this.gridToCsv(this.gridSource);
+				filename = this.gridBasename + '.csv';
+			} else if (btn.exportType == 'store') {
+				csv += this.storeToCsv(this.storeSource);
+				filename = this.storeBasename + '.csv';
+			}
+		}
+		let blob = new Blob([csv],{type:"text/csv;charset=utf-8"});
+		if (window.navigator.msSaveOrOpenBlob){
+			navigator.msSaveOrOpenBlob(blob,filename);
+		} else {
+			let a = window.document.createElement("a");
+			a.style.display= "none";
+			a.href = window.URL.createObjectURL(blob);
+			a.download = filename;
+			document.body.appendChild(a);
+			a.click();
+			document.body.removeChild(a);
+		}
+	},
+	
+	menuBeforeShow: function (menu) {
+		menu.items.each(function(item) {
+			switch (item.exportType) {
+				case 'grid':
+					item.setDisabled(this.gridSource == undefined); 
+					break;
+				case 'store':
+					item.setDisabled(this.storeSource == undefined); 
+					break;
+			}
+		},this);
+	},
+	
+	menuItemClick: function (item) {
+		let csv = "\ufeff";
+		let filename = "";
+		if (item.exportFormat == 'csv'){
+			if (item.exportType == 'grid') {
+				csv += this.gridToCsv(this.gridSource);
+				filename = this.gridBasename + '.csv';
+			} else if (item.exportType == 'store') {
+				csv += this.storeToCsv(this.storeSource);
+				filename = this.storeBasename + '.csv';
+			}
+		}
+		if (window.navigator.msSaveOrOpenBlob){
+			let blob = new Blob([csv],{type:"text/csv;charset=utf-8"});
+			navigator.msSaveOrOpenBlob(blob,filename);
+		} else {
+			item.el.dom.setAttribute('download',filename);
+			let href = encodeURI("data:text/csv;charset=utf-8," + csv)
+			item.el.dom.setAttribute('href',href);
+			item.el.dom.setAttribute('target','_self');
+		}
+	},
+	
+	gridToCsv: function (grid) {
+		let csv = "";
+		let view = grid.getView();
+		let store = grid.store;
+		
+		// Get the array of all grid columns
+		let columns = grid.getColumnModel().getColumnsBy(function(c) {return true} );
+		
+		// headerArray[] will hold data for the CSV header row
+		let headerArray = [];
+		// ci[] will hold the column indexes for which we will later get row data
+		let ci = [];
+		for (let x=0; x < columns.length; x++){
+			let c = columns[x];
+			// Criteria for inclusion of the column
+			if (c.dataIndex != "" && c.header != "" && !c.hidden){
+				// Build an element to hold the column header's HTML
+				let el = document.createElement('html');
+				el.innerHTML = c.header;
+				// Try to find the first child element with an 'exportvalue' attribute
+				let ev = el.querySelector('[exportvalue]');
+				if (ev != null) {
+					// An element with an 'exportvalue' attribute was found. The CSV column header will be the value of 'exportvalue'
+					headerArray.push('"' + ev.getAttribute('exportvalue') + '"');
+				} else {
+					// No element with an 'exportvalue' attribute was found was found. The CSV column header will be the quoted UI column header
+					headerArray.push('"' + c.header + '"');
+				}
+				// Add this column index to ci[]
+				ci.push(x);
+			}
+		}
+		// Comma separate the header data and append to the CSV 
+		csv += headerArray.join(',') + "\n";
+		
+		// Process all the records in the grid's store
+		let recordCount = store.getCount();
+		for (let x=0; x < recordCount; x++){
+			// rowCells[] is an array of <td> children of the first <tr> element of a row
+			let rowCells = view.getRow(x).getElementsByTagName('tr')[0].cells;
+			// rowArray[] will hold data for a single CSV row
+			let rowArray = [];
+			// Iterate across the included column indexes 
+			for (let x=0; x < ci.length; x++){
+				// Try to find the first child element with an 'exportvalue' attribute
+				let ev = rowCells[ci[x]].querySelector('[exportvalue]');
+				if (ev != null) {
+					// An element with an 'exportvalue' attribute was found was found. The CSV data will be the value of 'exportvalue'
+					rowArray.push('"' + ev.getAttribute('exportvalue') + '"');
+				} else {
+					// No element with an 'exportvalue' attribute was found was found. The CSV data will be the quoted and escaped textContent of the <td>'s firstChild
+					let value = '"' + rowCells[ci[x]].firstChild.textContent.replace(/"/g,'""') + '"';
+					rowArray.push(value);
+				}
+			}
+			// Comma separate the row data and append to the CSV 
+			csv += rowArray.join(',') + "\n";
+		}
+		
+		return csv;	
+	},
+	
+	storeToCsv: function (store) {
+		let csv = "";
+		
+		// headerArray[] will hold data for the CSV header row
+		let headerArray = [];
+		// fieldArray[] will hold data for later traversal of the Ext.data.Records
+		let fieldArray = [];
+		store.fields.each(function(item){
+			headerArray.push('"' + item.name + '"');
+			fieldArray.push(item.name);
+		});
+		// Comma separate the header data and append to the CSV 
+		csv += headerArray.join(',') + "\n";
+
+		store.each(function(record){
+			let rowArray = [];
+			for (let x = 0; x < fieldArray.length; x++){
+				let field = fieldArray[x];
+				let value = "";
+				switch (Object.prototype.toString.call(record.data[field])) {
+					case "[object String]":
+						value = '"' + record.data[field] + '"';
+						break;
+					case "[object Number]":
+						value = record.data[field];
+						break;
+					case "[object Date]":
+						value = record.data[field].format("Y-m-d H:i:s");
+						break;
+					default:
+						value = record.data[field];
+						break;
+				}
+				rowArray.push(value);
+			}
+			csv += rowArray.join(',') + "\n";
+		});
+		
+		return csv;	
+	}
+
+
+	
+});
+Ext.reg('exportbutton', Ext.ux.ExportButton);
---
+++ clients/extjs/js/completionStatus.js
@@ -0,0 +1,427 @@
+/*
+$Id: completionStatus.js 807 2017-07-27 13:04:19Z csmig $
+*/
+
+function addCompletionStatus(packageId,packageName) {
+
+	var groupRow = [
+		{header: ' ', colspan: 2, align: 'center'},
+		{header: 'Total', colspan: 2, align: 'center'},
+		{header: 'Manual', colspan: 2, align: 'center'},
+		{header: 'SCAP', colspan: 2, align: 'center'},
+		{header: 'Findings', colspan: 3, align: 'center'}
+	];
+
+	var summary = new Ext.ux.grid.GroupSummary();
+
+	var completionGrid = new Ext.grid.GridPanel({
+		id: 'completionGrid-' + packageId,
+        plugins: summary,
+		//title: 'Checklist Status (' + packageName + ')',
+		region:'center',
+		layout:'fit',
+		store: new Ext.data.GroupingStore ({
+			url: 'pl/getCompletionStatus.pl',
+			sortInfo: {
+				field: 'assetName'
+			},
+			groupField: 'assetName',
+			reader: new Ext.data.JsonReader({
+				root: 'rows',
+				totalProperty: 'records',
+				fields: [
+					{name:'id',type:'int'},
+					{name:'assetId',type:'int'},
+					{name:'stigId',type:'string'},
+					{name:'revId',type:'string'},
+
+					{name:'checksManual',type:'int'},
+					{name:'checksScap',type:'int'},
+					{name:'checksTotal',type:'int'},
+					
+					{name:'unreviewedManual',type:'int'},
+					{name:'unreviewedScap',type:'int'},
+					{name:'unreviewedTotal',type:'int'},
+
+					{name:'inProgressManual',type:'int'},
+					{name:'inProgressScap',type:'int'},
+					{name:'inProgressTotal',type:'int'},
+
+					{name:'submittedManual',type:'int'},
+					{name:'submittedScap',type:'int'},
+					{name:'submittedTotal',type:'int'},
+
+					{name:'rejectedManual',type:'int'},
+					{name:'rejectedScap',type:'int'},
+					{name:'rejectedTotal',type:'int'},
+
+					{name:'approvedManual',type:'int'},
+					{name:'approvedScap',type:'int'},
+					{name:'approvedTotal',type:'int'},
+
+					{name:'cat1Count',type:'int'},
+					{name:'cat2Count',type:'int'},
+					{name:'cat3Count',type:'int'},
+					'assetName','stigTitle'
+				]
+			}),
+			listeners: {
+				load: function (store,records) {
+					Ext.getCmp('completionGrid-' + packageId + '-totalText').setText(records.length + ' records');
+				}
+			}
+		}),
+		columns: [
+			{header: "Asset",width:35,dataIndex:'assetName',sortable:true,
+				summaryRenderer: function(v, params, data){
+                    return "Checklist totals";
+                },
+			},
+			{header: "Checklist",width:35,dataIndex:'stigId',sortable:true,id:'completionGrid-'+ packageId + 'becnhmarkId',
+				summaryRenderer: function(v, params, data){
+                    return "Asset totals";
+                },
+			},
+			{header: "Checks",width:10,dataIndex:'checksTotal',sortable:true,align:'right',renderer:renderGrey,summaryType: 'sum'},
+			{header: "Not Reviewed",width:10,dataIndex:'unreviewedTotal',sortable:true,align:'right',renderer:renderCat23,summaryType: 'sum'},
+			{header: "In Progress",width:10,dataIndex:'inProgressTotal',sortable:true,align:'right',renderer:renderGrey,summaryType: 'sum'},
+			{header: "<img src=img/ready-16.png width=12 height=12> Submitted",width:10,dataIndex:'submittedTotal',sortable:true,align:'right',renderer:renderGrey,summaryType: 'sum'},
+			{header: "<img src=img/rejected-16.png width=12 height=12> Returned",width:10,dataIndex:'rejectedTotal',sortable:true,align:'right',renderer:renderGrey,summaryType: 'sum'},
+			{header: "<img src=img/lock-16.png width=12 height=12> Approved",width:10,dataIndex:'approvedTotal',sortable:true,align:'right',renderer:renderGrey,summaryType: 'sum'},
+			{header: "Cat 1",width:10,dataIndex:'cat1Count',sortable:true,align:'right',renderer:renderCat1,summaryType: 'sum'},			
+			{header: "Cat 2",width:10,dataIndex:'cat2Count',sortable:true,align:'right',renderer:renderCat23,summaryType: 'sum'},			
+			{header: "Cat 3",width:10,dataIndex:'cat3Count',sortable:true,align:'right',renderer:renderCat23,summaryType: 'sum'}
+			
+		],
+		//autoExpandColumn:'completionGrid-'+ packageId + 'becnhmarkId',
+		//border: false,
+		//style: {
+		//	borderBottomWidth: "1px"
+		//},
+		loadMask: true,
+		stripeRows: true,
+		view: new Ext.grid.GroupingView({
+			enableGrouping:true,
+			hideGroupedColumn: true,
+			forceFit:true,
+			emptyText: 'No records found.',
+			groupTextTpl: '{text} ({[values.rs.length]} {[values.text.split(":")[0] == "Asset" ? "checklist" : "asset"]}{[values.rs.length > 1 ? "s assigned" : " assigned"]})',
+			getRowClass: function(record, rowIndex, rp, ds){ // rp = rowParams
+				if (record.data.stigTitle == '!! NO STIG ASSIGNMENTS !!') {
+					return 'sm-grid3-row-black';
+				}
+				// if(record.data.noreviewCount == '0'){
+					// return 'sm-grid3-row-green';
+				// } else if (record.data.noreviewCount != record.data.checkCount) {
+					// return 'sm-grid3-row-orange';
+				// } else {
+					// return 'sm-grid3-row-red';
+				// }
+			}
+		}),
+		sm: new Ext.grid.RowSelectionModel ({
+		}),
+		listeners: {
+			rowdblclick: {
+				fn: function(grid,rowIndex,e) {
+					var r = grid.getStore().getAt(rowIndex);
+					if (r.get('stigTitle') != '!! NO STIG ASSIGNMENTS !!') {
+						Ext.getCmp('main-tabs').setActiveTab('tab-reviews');
+						var n = new Object();
+						var attributes = new Object();
+						attributes.report = "review";
+						attributes.assetId = r.get('assetId');
+						attributes.assetName = r.get('assetName');
+						attributes.stigId = r.get('stigId');
+						attributes.stigName = r.get('stigId');
+						attributes.revId = r.get('revId');
+						n.attributes = attributes;
+						reviewsTreeClick(n);
+					}
+					//Ext.getCmp('groupGrid').getStore().load({params:{assetId:assetId, revId:revId}});
+				}
+			}
+		},
+		tbar: new Ext.Toolbar({
+			items: [
+				// START Grouping control
+				{
+					xtype: 'buttongroup',
+					title: 'Grouping',
+					items: [
+					{
+						xtype: 'tbbutton',
+						icon: 'img/security_firewall_on.png',
+						tooltip: 'Group by STIG',
+						toggleGroup: 'completionGrid-groupBy' + packageId,
+						enableToggle:true,
+						allowDepress: false,
+						width: 20,
+						handler: function(btn){
+							if (btn.pressed) {
+								Ext.getCmp('completionGrid-expandButton' + packageId).enable();
+								Ext.getCmp('completionGrid-collapseButton' + packageId).enable();
+								completionGrid.getView().group
+								completionGrid.getStore().groupBy('stigId');
+							} else {
+								Ext.getCmp('completionGrid-expandButton' + packageId).disable();
+								Ext.getCmp('completionGrid-collapseButton' + packageId).disable();
+								completionGrid.getStore().clearGrouping();
+							}
+						}
+					},{
+						xtype: 'tbbutton',
+						icon: 'img/mycomputer1-16.png',
+						tooltip: 'Group by asset',
+						toggleGroup: 'completionGrid-groupBy' + packageId,
+						enableToggle:true,
+						allowDepress: false,
+						pressed: true,
+						width: 20,
+						handler: function(btn){
+							if (btn.pressed) {
+								Ext.getCmp('completionGrid-expandButton' + packageId).enable();
+								Ext.getCmp('completionGrid-collapseButton' + packageId).enable();
+								completionGrid.getStore().groupBy('assetName');
+							} else {
+								Ext.getCmp('completionGrid-expandButton' + packageId).disable();
+								Ext.getCmp('completionGrid-collapseButton' + packageId).disable();
+								completionGrid.getStore().clearGrouping();
+							}
+						}
+					},{
+						xtype: 'tbseparator'
+					},{
+						xtype: 'tbbutton',
+						//icon: 'img/chevron.png',
+						icon: 'img/minus-grey.png',
+						id: 'completionGrid-collapseButton' + packageId,
+						tooltip: 'Collapse all groups',
+						width: 20,
+						handler: function(btn){
+							completionGrid.getView().collapseAllGroups();
+						}
+					},{
+						xtype: 'tbbutton',
+						//icon: 'img/chevron_expand.png',
+						icon: 'img/plus-grey.png',
+						id: 'completionGrid-expandButton' + packageId,
+						tooltip: 'Expand all groups',
+						width: 20,
+						handler: function(btn){
+							completionGrid.getView().expandAllGroups();
+						}
+					}]
+				// END Grouping control
+				}
+				// ,{
+				// // START Filter control
+					// xtype: 'buttongroup',
+					// title: 'Filtering',
+					// items: [
+					// {
+						// xtype: 'tbbutton',
+						// icon: 'img/security_firewall_on.png',
+						// tooltip: 'Filter by STIG id',
+						// toggleGroup: 'completionGrid-filterBy' + packageId,
+						// enableToggle:true,
+						// allowDepress: true,
+						// pressed: false,
+						// width: 20,
+						// handler: function(btn){
+							// var filterField = Ext.getCmp('completionGrid-filterField' + packageId);
+							// if (btn.pressed) {
+								// filterField.enable();
+								// completionGrid.getStore().filterField = 'stigId';
+								// if (filterField.getRawValue() == '') {
+									// filterField.emptyText = 'Enter a STIG filter...';
+									// filterField.setRawValue(filterField.emptyText);
+								// } else {
+									// filterField.emptyText = 'Enter a STIG filter...';
+								// }
+								// filterStatusChangesStore();
+							// } else {
+								// filterField.disable();
+								// //filterField.setValue('');
+								// filterStatusChangesStore();
+							// }
+						// }
+					// },{
+						// xtype: 'tbbutton',
+						// icon: 'img/mycomputer1-16.png',
+						// tooltip: 'Filter by asset name',
+						// toggleGroup: 'completionGrid-filterBy' + packageId,
+						// enableToggle:true,
+						// allowDepress: true,
+						// width: 20,
+						// handler: function(btn){
+							// var filterField = Ext.getCmp('completionGrid-filterField' + packageId);
+							// if (btn.pressed) {
+								// filterField.enable();
+								// completionGrid.getStore().filterField = 'assetName';
+								// if (filterField.getRawValue() == '') {
+									// filterField.emptyText = 'Enter an asset filter...';
+									// filterField.setRawValue(filterField.emptyText);
+								// } else {
+									// filterField.emptyText = 'Enter an asset filter...';
+								// }
+								// filterStatusChangesStore();
+							// } else {
+								// filterField.disable();
+								// //filterField.setValue('');
+								// filterStatusChangesStore();
+							// }
+						// }
+					// },{
+						// xtype: 'tbseparator'
+					// },{
+						// xtype: 'trigger',
+						// fieldLabel: 'Filter',
+						// triggerClass: 'x-form-clear-trigger',
+						// onTriggerClick: function() {
+							// this.triggerBlur();
+							// this.blur();
+							// this.setValue('');
+							// filterStatusChangesStore();
+						// },
+						// id: 'completionGrid-filterField' + packageId,
+						// width: 140,
+						// submitValue: false,
+						// disabled: true,
+						// enableKeyEvents:true,
+						// emptyText:'Filter string...',
+						// listeners: {
+							// keyup: function (field,e) {
+								// filterStatusChangesStore();
+								// return false;
+							// }
+						// }
+					// }
+					// ]
+				// }
+			]
+		}),
+		bbar: new Ext.Toolbar({
+			items: [
+			{
+				xtype: 'tbbutton',
+				iconCls: 'icon-refresh',
+				tooltip: 'Reload this grid',
+				width: 20,
+				handler: function(btn){
+					completionGrid.getStore().reload();
+				}
+			},{
+				xtype: 'tbseparator'
+			}
+			// ,{
+				// xtype: 'tbbutton',
+				// iconCls: 'icon-excel',
+				// tooltip: 'Download an enhanced Audits report spreadsheet',
+				// //text: 'Enhanced',
+				// width: 20,
+				// handler: function(btn){
+					// var ourStore = completionGrid.getStore();
+					// var lo = ourStore.lastOptions;
+					// window.location=ourStore.url + '?xls=1&packageId=' + lo.params.packageId;
+				// }
+			// }
+			,{
+				xtype: 'tbbutton',
+				iconCls: 'icon-save',
+				width: 20,
+				tooltip: 'Download this table\'s data as Comma Separated Values (CSV)',
+				handler: function(btn){
+					var ourStore = completionGrid.getStore();
+					var lo = ourStore.lastOptions;
+					window.location=ourStore.url + '?csv=1&packageId=' + lo.params.packageId;
+				}
+			},{
+				xtype: 'tbfill'
+			},{
+				xtype: 'tbseparator'
+			},{
+				xtype: 'tbtext',
+				id: 'completionGrid-' + packageId + '-totalText',
+				text: '0 records',
+				width: 80
+			}]
+		})
+	});
+	
+	var thisTab = Ext.getCmp('reports-center-tab').add({
+		id: 'completionTab-' + packageId,
+		iconCls: 'sm-report-icon',
+		title: 'Completion Status (' + packageName + ')',
+		closable:true,
+		layout: 'border',
+		items: [completionGrid]
+	});
+	thisTab.show();
+	
+	completionGrid.getStore().load({params:{packageId: packageId}});
+
+}; //end addCompletionReport();
+
+function renderGrey(value, metaData, record, rowIndex, colIndex, store) {
+	metaData.css = 'sm-cell-grey';
+	return value;
+}
+function renderDays(value, metaData, record, rowIndex, colIndex, store) {
+	metaData.css = 'sm-cell-grey';
+	if (isNaN(value)) {
+		return '-';
+	} else if (value == 0) {
+		return 'Today';
+	} else if (value == 1) {
+		return value + ' day';
+	} else {
+		return value + ' days';
+	}
+}
+
+function renderCat1(value, metaData, record, rowIndex, colIndex, store) {
+	if (value > 0) {
+		metaData.css = 'sm-cell-red';
+	} else {
+		metaData.css = 'sm-cell-green';
+	}
+	return value;
+}
+
+function renderCat23(value, metaData, record, rowIndex, colIndex, store) {
+	if (value > 0) {
+		metaData.css = 'sm-cell-orange';
+	} else {
+		metaData.css = 'sm-cell-green';
+	}
+	return value;
+}
+
+function renderUndone(value, metaData, record, rowIndex, colIndex, store) {
+	if (value == 0) {
+		metaData.css = 'sm-cell-green';
+	} else {
+		metaData.css = 'sm-cell-red';
+	}
+	return value
+}
+function renderDone(value, metaData, record, rowIndex, colIndex, store) {
+	if (value == record.data.totalCheckCount) {
+		metaData.css = 'sm-cell-green';
+	} else {
+		metaData.css = 'sm-cell-red';
+	}
+	return value
+}
+
+function renderChecklist(value, metaData, record, rowIndex, colIndex, store) {
+	metaData.css = 'sm-cell-checklist';
+	return value;
+}
+
+function renderAsset(value, metaData, record, rowIndex, colIndex, store) {
+	metaData.css = 'sm-cell-asset';
+	return value;
+}
+
