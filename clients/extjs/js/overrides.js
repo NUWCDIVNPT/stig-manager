9bba16c9182e54fb638bd0b8baa401b2b9eac8ec
csmig <carlsmigielski@gmail.com>
2020-04-22 09:25:43 -0400

    Overrides for tab close and remove href='#'

--- clients/extjs/js/overrides.js
+++ clients/extjs/js/overrides.js
@@ -471,3 +471,108 @@ Ext.override(Ext.QuickTip, {
         }
     }
 });
+
+// Plugin to TabPanel that allows tabs to be closed with middle-click (mouse wheel)
+// In TabPanel configuration, set "plugins: new Ext.ux.TabCloseOnMiddleClick()"
+// Source: Unknown
+Ext.ux.TabCloseOnMiddleClick = function(){
+    let tabs;
+    function onMouseDown(e){
+        e.preventDefault()
+        let t = tabs.findTargets(e), b = e.browserEvent.button, w = e.browserEvent.which, 
+        isMiddleButtonPressed = (w === null || w === undefined) ? b==1 : w==2; // browser dependent: http://unixpapa.com/js/mouse.html
+        
+        if (isMiddleButtonPressed && t && t.item) {
+            if (t.item.closable) {
+                tabs.remove(t.item)
+            } else {
+                e.stopPropagation()
+            }
+        }
+    } 
+    function onRender(){
+        tabs.strip.on({mousedown: onMouseDown})
+    }
+    this.init = function(tp){
+        tabs = tp
+        tabs.on('render', onRender)
+    }
+}
+
+// GridView rendered without href="#", which supresses Chrome status bar
+// Source: Carl Smigielski
+Ext.override(Ext.grid.GridView,{
+    masterTpl: new Ext.Template(
+        '<div class="x-grid3" hidefocus="true">',
+            '<div class="x-grid3-viewport">',
+                '<div class="x-grid3-header">',
+                    '<div class="x-grid3-header-inner">',
+                        '<div class="x-grid3-header-offset" style="{ostyle}">{header}</div>',
+                    '</div>',
+                    '<div class="x-clear"></div>',
+                '</div>',
+                '<div class="x-grid3-scroller">',
+                    '<div class="x-grid3-body" style="{bstyle}">{body}</div>',
+                    '<a class="x-grid3-focus" tabIndex="-1"></a>',
+                '</div>',
+            '</div>',
+            '<div class="x-grid3-resize-marker">&#160;</div>',
+            '<div class="x-grid3-resize-proxy">&#160;</div>',
+        '</div>'
+    )
+})
+
+// TreeNodeUI rendered without href="#", which supresses Chrome status bar
+// Source: Carl Smigielski
+Ext.override(Ext.tree.TreeNodeUI,{
+    renderElements : function(n, a, targetNode, bulkRender){
+        // add some indent caching, this helps performance when rendering a large tree
+        this.indentMarkup = n.parentNode ? n.parentNode.ui.getChildIndent() : '';
+
+        var cb = Ext.isBoolean(a.checked),
+            nel,
+            href = this.getHref(a.href),
+            buf = ['<li class="x-tree-node"><div ext:tree-node-id="',n.id,'" class="x-tree-node-el x-tree-node-leaf x-unselectable ', a.cls,'" unselectable="on">',
+            '<span class="x-tree-node-indent">',this.indentMarkup,"</span>",
+            '<img alt="" src="', this.emptyIcon, '" class="x-tree-ec-icon x-tree-elbow" />',
+            '<img alt="" src="', a.icon || this.emptyIcon, '" class="x-tree-node-icon',(a.icon ? " x-tree-node-inline-icon" : ""),(a.iconCls ? " "+a.iconCls : ""),'" unselectable="on" />',
+            cb ? ('<input class="x-tree-node-cb" type="checkbox" ' + (a.checked ? 'checked="checked" />' : '/>')) : '',
+            '<a hidefocus="on" class="x-tree-node-anchor" tabIndex="1" ',
+             a.hrefTarget ? ' target="'+a.hrefTarget+'"' : "", '><span unselectable="on">',n.text,"</span></a></div>",
+            '<ul class="x-tree-node-ct" style="display:none;"></ul>',
+            "</li>"].join('');
+
+        if(bulkRender !== true && n.nextSibling && (nel = n.nextSibling.ui.getEl())){
+            this.wrap = Ext.DomHelper.insertHtml("beforeBegin", nel, buf);
+        }else{
+            this.wrap = Ext.DomHelper.insertHtml("beforeEnd", targetNode, buf);
+        }
+
+        this.elNode = this.wrap.childNodes[0];
+        this.ctNode = this.wrap.childNodes[1];
+        var cs = this.elNode.childNodes;
+        this.indentNode = cs[0];
+        this.ecNode = cs[1];
+        this.iconNode = cs[2];
+        var index = 3;
+        if(cb){
+            this.checkbox = cs[3];
+            // fix for IE6
+            this.checkbox.defaultChecked = this.checkbox.checked;
+            index++;
+        }
+        this.anchor = cs[index];
+        this.textNode = cs[index].firstChild;
+    }
+})
+
+// TabPanel rendered without href="#", which supresses Chrome status bar
+// Source: Carl Smigielski
+Ext.override(Ext.TabPanel, {
+    itemTpl: new Ext.Template(
+        '<li class="{cls}" id="{id}"><a class="x-tab-strip-close"></a>',
+        '<a class="x-tab-right"><em class="x-tab-left">',
+        '<span class="x-tab-strip-inner"><span class="x-tab-strip-text {iconCls}">{text}</span></span>',
+        '</em></a></li>'
+   )
+})
