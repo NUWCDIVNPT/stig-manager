2989508413c13b9feb7620385680461f5b7fefd7
csmig <carlsmigielski@gmail.com>
2020-04-26 19:22:37 -0400

    User endpoints access control

--- api/service/controllers/User.js
+++ api/service/controllers/User.js
@@ -5,14 +5,36 @@ const config = require('../utils/config')
 const User = require(`../service/${config.database.type}/UserService`)
 
 module.exports.createUser = async function createUser (req, res, next) {
-  let body = req.swagger.params['body'].value
-  let projection = req.swagger.params['projection'].value
-  try {
-    let response = await User.createUser(body, projection, req.userObject)
-    writer.writeJson(res, response)
+  if ( req.userObject.canAdmin ) {
+    let body = req.swagger.params['body'].value
+    let projection = req.swagger.params['projection'].value
+    try {
+      let response = await User.createUser(body, projection, req.userObject)
+      writer.writeJson(res, response)
+    }
+    catch(err) {
+      writer.writeJson(res, err)
+    }
   }
-  catch(err) {
-    writer.writeJson(res, err)
+  else {
+    writer.writeJson(res, writer.respondWithCode ( 403, {message: `User has insufficient privilege to complete this request.`} ) )    
+  }
+}
+
+module.exports.deleteUser = async function deleteUser (req, res, next) {
+  if ( req.userObject.canAdmin ) {
+    let body = req.swagger.params['body'].value
+    let projection = req.swagger.params['projection'].value
+    try {
+      let response = await User.deleteUser(body, projection, req.userObject)
+      writer.writeJson(res, response)
+    }
+    catch(err) {
+      writer.writeJson(res, err)
+    }
+  }
+  else {
+    writer.writeJson(res, writer.respondWithCode ( 403, {message: `User has insufficient privilege to complete this request.`} ) )    
   }
 }
 
@@ -25,19 +47,79 @@ module.exports.getUserObject = async function getUserObject (req, res, next) {
   }
 }
 
-module.exports.getUsers = async function getUsers (req, res, next) {
+module.exports.getUserByUserId = async function getUserByUserId (req, res, next) {
+  let userId = req.swagger.params['userId'].value
   let projection = req.swagger.params['projection'].value
-  let elevate = req.swagger.params['elevate'].value
-  let role = req.swagger.params['role'].value
-  let packageId = req.swagger.params['packageId'].value
-  let benchmarkId = req.swagger.params['benchmarkId'].value
-  let dept = req.swagger.params['dept'].value
-  let canAdmin = req.swagger.params['canAdmin'].value
   try {
-    let response = await User.getUsers(projection, elevate, role, packageId, benchmarkId, dept, canAdmin, req.userObject)
+    let response = await User.getUserByUserId(userId, projection, req.userObject)
     writer.writeJson(res, response)
   }
   catch(err) {
     writer.writeJson(res, err)
   }
 }
+
+module.exports.getUsers = async function getUsers (req, res, next) {
+  if ( req.userObject.canAdmin || req.userObject.role == 'IAO' || req.userObject.role == 'Staff') {
+    let projection = req.swagger.params['projection'].value
+    let elevate = req.swagger.params['elevate'].value
+    let role = req.swagger.params['role'].value
+    let dept = req.swagger.params['dept'].value
+    let canAdmin = req.swagger.params['canAdmin'].value
+    try {
+      let response = await User.getUsers(projection, elevate, role, packageId, benchmarkId, dept, canAdmin, req.userObject)
+      writer.writeJson(res, response)
+    }
+    catch(err) {
+      writer.writeJson(res, err)
+    }
+  }
+  else {
+    writer.writeJson(res, writer.respondWithCode ( 403, {message: `User has insufficient privilege to complete this request.`} ) )    
+  }
+}
+
+module.exports.replaceUser = async function replaceUser (req, res, next) {
+  if ( req.userObject.canAdmin || req.userObject.role == 'IAO' || req.userObject.role == 'Staff') {
+    let userId = req.swagger.params['userId'].value
+    let body = req.swagger.params['body'].value
+    let projection = req.swagger.params['projection'].value
+    if ( !req.userObject.canAdmin ) {
+      let prohibited = ['username', 'dept', 'role', 'canAdmin']
+      let hasProhibited = Object.keys(body).some((val) => prohibited.includes(val))
+      if (hasProhibited) {
+        writer.writeJson(res, writer.respondWithCode ( 403, {message: `User has insufficient privilege to complete this request.`} ) )
+      }
+    }
+    try {
+      let response = await User.replaceUser(userId, body, projection, req.userObject)
+      writer.writeJson(res, response)
+    }
+    catch(err) {
+      writer.writeJson(res, err)
+    }
+  }
+}
+
+module.exports.updateUser = async function updateUser (req, res, next) {)
+  if ( req.userObject.canAdmin || req.userObject.role == 'IAO' || req.userObject.role == 'Staff') {
+    let userId = req.swagger.params['userId'].value
+    let body = req.swagger.params['body'].value
+    let projection = req.swagger.params['projection'].value
+    if ( !req.userObject.canAdmin ) {
+      let prohibited = ['username', 'dept', 'role', 'canAdmin']
+      let hasProhibited = Object.keys(body).some((val) => prohibited.includes(val))
+      if (hasProhibited) {
+        writer.writeJson(res, writer.respondWithCode ( 403, {message: `User has insufficient privilege to complete this request.`} ) )
+      }
+    }
+    try {
+      let response = await User.updateUser(userId, body, projection, req.userObject)
+      writer.writeJson(res, response)
+    }
+    catch(err) {
+      writer.writeJson(res, err)
+    }
+  }
+}
+
