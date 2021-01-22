8ce84621773a4961438a918088b8dc0753cd9ba1
csmig <33138761+csmig@users.noreply.github.com>
2021-01-22 10:51:26 -0500

    fix: API issues #97 #98 (#111)
    * fix: GET STIG groups returning 500 (#97)
    
    * fix: GET STIG revision returns 500 (#98)

--- api/source/service/mysql/utils.js
+++ api/source/service/mysql/utils.js
@@ -142,13 +142,13 @@ module.exports.parseRevisionStr = function (revisionStr) {
     let results = /V(\d+)R(\d+(\.\d+)?)/.exec(revisionStr)
     ro.version = results[1]
     ro.release = results[2]
-    ro.table = 'stig.revision'
+    ro.table = 'revision'
     ro.table_alias = 'r'
     ro.predicates = ' and r.version = ? and r.release = ? '
   } else {
     ro.version = null
     ro.release = null
-    ro.table = 'stig.current_rev'
+    ro.table = 'current_rev'
     ro.table_alias = 'cr'
     ro.predicates = ''
   }
