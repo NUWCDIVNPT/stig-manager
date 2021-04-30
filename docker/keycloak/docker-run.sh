282bffc0b3662316977ddfbe91fae4b08415e427
csmig <33138761+csmig@users.noreply.github.com>
2021-04-30 10:03:08 -0400

    chore: remove obsolete docker dir (#278)

--- docker/keycloak/docker-run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker container rm stig-manager-auth --force
-docker run --name stig-manager-auth -p 8080:8080 -p 8443:8443 -d stig-manager-auth:${1:-dev}
--- docker/stigman-init/db/create-admin.sql
+++
@@ -1,5 +0,0 @@
-prompt connecting to stigman
-connect stigman/stigman@orclpdb1;
-Insert into stigman.USER_DATA (CN,NAME,DEPT,ROLEID,CANADMIN) values ('admin','admin',1,4,1);
-commit;
-exit
