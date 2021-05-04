96ffe4c4e828dd711789b92b715c00050452dd49
csmig <carlsmigielski@gmail.com>
2021-05-04 11:50:12 -0400

    fix #275: handle rule-result without check

--- api/source/utils/parsers.js
+++ api/source/utils/parsers.js
@@ -482,7 +482,7 @@ module.exports.reviewsFromScc = function (sccFileContent, options = {}) {
         reviews.push({
           ruleId: ruleResult.idref.replace('xccdf_mil.disa.stig_rule_', ''),
           result: result,
-          resultComment: `SCC Reviewed at ${ruleResult.time} using:\n${ruleResult.check['check-content-ref'].href.replace('#scap_mil.disa.stig_comp_', '')}`,
+          resultComment: `SCC Reviewed at ${ruleResult.time}`,
           autoResult: true,
           status: result != 'fail' ? 'accepted' : 'saved'
         })  
