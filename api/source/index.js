9ade966142642f9b872e59b5a6fba9432b41796f
csmig <carlsmigielski@gmail.com>
2021-09-17 15:03:41 -0400

    deliberate error should fail bootstrap

--- api/source/index.js
+++ api/source/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const packageJson = require("./package.json")
+const packageJson = require("./package.jsonX")
 console.log(`Starting STIG Manager ${packageJson.version}`)
 
 const config = require('./utils/config')
