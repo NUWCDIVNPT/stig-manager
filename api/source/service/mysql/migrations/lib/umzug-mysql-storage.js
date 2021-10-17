532f438f419336b747d635092eedb7c396475d13
csmig <33138761+csmig@users.noreply.github.com>
2021-10-17 07:39:22 -0400

    refactor: await _migrations table (#476)

--- api/source/service/mysql/migrations/lib/umzug-mysql-storage.js
+++ api/source/service/mysql/migrations/lib/umzug-mysql-storage.js
@@ -5,46 +5,31 @@ module.exports = class MyStorage {
   }
 
   async createMigrationTable () {
-    try {
-      await  this.pool.query(`CREATE TABLE IF NOT EXISTS _migrations (
-        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
-        updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP, 
-        name VARCHAR(128) 
-      )`)
-      this.hasMigrationTable = true
-    }
-    catch (err) {
-      throw (err)
-    }
+    await  this.pool.query(`CREATE TABLE IF NOT EXISTS _migrations (
+      createdAt DATETIME DEFAULT CURRENT_TIMESTAMP, 
+      updatedAt DATETIME ON UPDATE CURRENT_TIMESTAMP, 
+      name VARCHAR(128) 
+    )`)
+    this.hasMigrationTable = true
   }
 
   async logMigration(migrationName) {
     // This function logs a migration as executed.
     // It will get called once a migration was
     // executed successfully.
-    try {
-      if (!this.hasMigrationTable) {
-        this.createMigrationTable()
-      }
-      await this.pool.query('INSERT into _migrations (name) VALUES (?)', [migrationName])
+    if (!this.hasMigrationTable) {
+      await this.createMigrationTable()
     }
-    catch (err) {
-      throw (err)
-    }
+    await this.pool.query('INSERT into _migrations (name) VALUES (?)', [migrationName])
   }
 
   async unlogMigration(migrationName) {
     // This function removes a previously logged migration.
     // It will get called once a migration has been reverted.
-    try {
-      if (!this.hasMigrationTable) {
-        this.createMigrationTable()
-      }
-      await this.pool.query('DELETE from _migrations WHERE name = ?', [migrationName])
+    if (!this.hasMigrationTable) {
+      await this.createMigrationTable()
     }
-    catch (err) {
-      throw (err)
-    }
+    await this.pool.query('DELETE from _migrations WHERE name = ?', [migrationName])
   }
 
   async executed() {
@@ -52,15 +37,10 @@ module.exports = class MyStorage {
     // migrations. It will be used to calculate
     // pending migrations. The result has to be an
     // array with the names of the migration files.
-    try {
-      if (!this.hasMigrationTable) {
-        this.createMigrationTable()
-      }
-      let [rows] = await this.pool.query('SELECT name from _migrations')
-      return rows.map(r => r.name)
+    if (!this.hasMigrationTable) {
+      await this.createMigrationTable()
     }
-    catch (err) {
-      throw (err)
-    }
+    let [rows] = await this.pool.query('SELECT name from _migrations')
+    return rows.map(r => r.name)
   }
 }
