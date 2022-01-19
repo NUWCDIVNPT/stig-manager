668196a25af3ec88ced2e0dbb398e406adf7f713
csmig <33138761+csmig@users.noreply.github.com>
2022-01-18 19:12:13 -0500

    chore: Build updates (#556)
    * update envvars to include logging
    * include migrations in build

--- api/launchers/stig-manager.sh
+++ api/launchers/stig-manager.sh
@@ -297,9 +297,33 @@
 #  Default: "false" Whether to fetch and import the current DISA STIG Library
 #  compilation from public.cyber.mil on initial database migration
 #
-#  Affects: API          
+#  Affects: API
 #==============================================================================
 # export STIGMAN_INIT_IMPORT_STIGS=
+
+#==============================================================================
+# STIGMAN_LOG_LEVEL
+#
+#  Default: "3" Controls the granularity of the generated log output, from 1 to
+#  4. Each level is inclusive of the ones before it. Level 1 will log only
+#  errors, level 2 includes warnings, level 3 includes status and transaction
+#  logs, and level 4 includes debug-level logs
+#
+#  Affects: API
+#==============================================================================
+# export STIGMAN_LOG_LEVEL=
+
+#==============================================================================
+# STIGMAN_LOG_MODE
+#
+#  Default: "combined" Controls whether the logs will create one “combined” log
+#  entry for http requests that includes both the request and response
+#  information; or two separate log entries, one for the request and one for the
+#  response, that can be correlated via a generated Request GUID in each entry
+#
+#  Affects: API
+#==============================================================================
+# export STIGMAN_LOG_MODE=
 
 #==============================================================================
 # STIGMAN_JWT_EMAIL_CLAIM
