bd2713ea9d0ae66a5a2583a1f48426efadcaf681
csmig <carlsmigielski@gmail.com>
2022-07-14 17:21:09 -0400

    prevent ctrl-a browser default action

--- client/src/js/stigman.js
+++ client/src/js/stigman.js
@@ -196,6 +196,13 @@ async function loadApp () {
 			width: 200,
 			dismissDelay: 60000 // Show while cursor is over element
 		})
+
+		window.addEventListener('keydown', function (e) {
+			// prevent ctrl-a from being handled by the browser
+			if (e.key === 'a' && e.ctrlKey) {
+				e.preventDefault()
+			}
+		})
 		
 	}
 	catch (e) {
