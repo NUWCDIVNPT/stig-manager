05498ab3087b8b3666983fa756355d89f0b02835
csmig <carlsmigielski@gmail.com>
2022-07-28 11:43:25 -0400

    tests fail when response validation fails

--- api/source/index.js
+++ api/source/index.js
@@ -278,7 +278,8 @@ function modulePathResolver( handlersPath, route, apiDoc ) {
 }
 
 function buildResponseValidationConfig() {
-  if ( config.settings.responseValidation == "logOnly" ){
+  if ( config.settings.responseValidation === "true" ) { return true }
+  if ( config.settings.responseValidation === "logOnly" ){
     return {
         onError: (error, body, req) => {
           logger.writeError('rest', 'responseValidation', {
