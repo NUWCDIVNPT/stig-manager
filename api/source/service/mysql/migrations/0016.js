48c95ae791849c091e1605220dd3389e7e03694e
cd-rite <61710958+cd-rite@users.noreply.github.com>
2022-08-26 16:03:19 -0400

    use cte in query

--- api/source/service/mysql/migrations/0016.js
+++ api/source/service/mysql/migrations/0016.js
@@ -32,94 +32,90 @@ const upMigration = [
   ADD COLUMN fixedResultEngine int  
   `,
 
-`
-  update stig_asset_map sam
-    inner join (
-      select
-        sa.assetId,
-        sa.benchmarkId,
-        min(review.ts) as minTs,
-        max(review.ts) as maxTs,  
-        
-        sum(CASE WHEN review.statusId = 0 THEN 1 ELSE 0 END) as saved,
-        sum(CASE WHEN review.resultEngine is not null and review.statusId = 0 THEN 1 ELSE 0 END) as savedResultEngine,
-        sum(CASE WHEN review.statusId = 1 THEN 1 ELSE 0 END) as submitted,
-        sum(CASE WHEN review.resultEngine is not null and review.statusId = 1 THEN 1 ELSE 0 END) as submittedResultEngine,
-        sum(CASE WHEN review.statusId = 2 THEN 1 ELSE 0 END) as rejected,
-        sum(CASE WHEN review.resultEngine is not null and review.statusId = 2 THEN 1 ELSE 0 END) as rejectedResultEngine,
-        sum(CASE WHEN review.statusId = 3 THEN 1 ELSE 0 END) as accepted,
-        sum(CASE WHEN review.resultEngine is not null and review.statusId = 3 THEN 1 ELSE 0 END) as acceptedResultEngine,
+  `
+  with source as
+    ( select
+       sa.assetId,
+       sa.benchmarkId,
+       min(review.ts) as minTs,
+       max(review.ts) as maxTs,  
+       
+       sum(CASE WHEN review.statusId = 0 THEN 1 ELSE 0 END) as saved,
+       sum(CASE WHEN review.resultEngine is not null and review.statusId = 0 THEN 1 ELSE 0 END) as savedResultEngine,
+       sum(CASE WHEN review.statusId = 1 THEN 1 ELSE 0 END) as submitted,
+       sum(CASE WHEN review.resultEngine is not null and review.statusId = 1 THEN 1 ELSE 0 END) as submittedResultEngine,
+       sum(CASE WHEN review.statusId = 2 THEN 1 ELSE 0 END) as rejected,
+       sum(CASE WHEN review.resultEngine is not null and review.statusId = 2 THEN 1 ELSE 0 END) as rejectedResultEngine,
+       sum(CASE WHEN review.statusId = 3 THEN 1 ELSE 0 END) as accepted,
+       sum(CASE WHEN review.resultEngine is not null and review.statusId = 3 THEN 1 ELSE 0 END) as acceptedResultEngine,
 
-		    sum(CASE WHEN review.resultId=4 and r.severity='high' THEN 1 ELSE 0 END) as highCount,
-        sum(CASE WHEN review.resultId=4 and r.severity='medium' THEN 1 ELSE 0 END) as mediumCount,
-        sum(CASE WHEN review.resultId=4 and r.severity='low' THEN 1 ELSE 0 END) as lowCount,
-
-		    sum(CASE WHEN review.resultId = 1 THEN 1 ELSE 0 END) as notchecked,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 1 THEN 1 ELSE 0 END) as notcheckedResultEngine,
-        sum(CASE WHEN review.resultId = 2 THEN 1 ELSE 0 END) as notapplicable,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 2 THEN 1 ELSE 0 END) as notapplicableResultEngine,
-        sum(CASE WHEN review.resultId = 3 THEN 1 ELSE 0 END) as pass,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 3 THEN 1 ELSE 0 END) as passResultEngine,
-        sum(CASE WHEN review.resultId = 4 THEN 1 ELSE 0 END) as fail,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 4 THEN 1 ELSE 0 END) as failResultEngine,
-        sum(CASE WHEN review.resultId = 5 THEN 1 ELSE 0 END) as unknown,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 5 THEN 1 ELSE 0 END) as unknownResultEngine,
-        sum(CASE WHEN review.resultId = 6 THEN 1 ELSE 0 END) as error,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 6 THEN 1 ELSE 0 END) as errorResultEngine,
-        sum(CASE WHEN review.resultId = 7 THEN 1 ELSE 0 END) as notselected,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 7 THEN 1 ELSE 0 END) as notselectedResultEngine,            
-        sum(CASE WHEN review.resultId = 8 THEN 1 ELSE 0 END) as informational,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 8 THEN 1 ELSE 0 END) as informationalResultEngine,
-        sum(CASE WHEN review.resultId = 9 THEN 1 ELSE 0 END) as fixed,
-        sum(CASE WHEN review.resultEngine is not null and review.resultId = 9 THEN 1 ELSE 0 END) as fixedResultEngine
-        
-      from
-        asset a
-        left join stig_asset_map sa using (assetId)
-        left join current_group_rule cgr using (benchmarkId)
-        left join rule r using (ruleId)
-        left join review on (r.ruleId=review.ruleId and review.assetId=sa.assetId)
+       sum(CASE WHEN review.resultId=4 and r.severity='high' THEN 1 ELSE 0 END) as highCount,
+       sum(CASE WHEN review.resultId=4 and r.severity='medium' THEN 1 ELSE 0 END) as mediumCount,
+       sum(CASE WHEN review.resultId=4 and r.severity='low' THEN 1 ELSE 0 END) as lowCount,
+       
+       sum(CASE WHEN review.resultId = 1 THEN 1 ELSE 0 END) as notchecked,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 1 THEN 1 ELSE 0 END) as notcheckedResultEngine,
+       sum(CASE WHEN review.resultId = 2 THEN 1 ELSE 0 END) as notapplicable,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 2 THEN 1 ELSE 0 END) as notapplicableResultEngine,
+       sum(CASE WHEN review.resultId = 3 THEN 1 ELSE 0 END) as pass,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 3 THEN 1 ELSE 0 END) as passResultEngine,
+       sum(CASE WHEN review.resultId = 4 THEN 1 ELSE 0 END) as fail,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 4 THEN 1 ELSE 0 END) as failResultEngine,
+       sum(CASE WHEN review.resultId = 5 THEN 1 ELSE 0 END) as unknown,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 5 THEN 1 ELSE 0 END) as unknownResultEngine,
+       sum(CASE WHEN review.resultId = 6 THEN 1 ELSE 0 END) as error,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 6 THEN 1 ELSE 0 END) as errorResultEngine,
+       sum(CASE WHEN review.resultId = 7 THEN 1 ELSE 0 END) as notselected,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 7 THEN 1 ELSE 0 END) as notselectedResultEngine,            
+       sum(CASE WHEN review.resultId = 8 THEN 1 ELSE 0 END) as informational,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 8 THEN 1 ELSE 0 END) as informationalResultEngine,
+       sum(CASE WHEN review.resultId = 9 THEN 1 ELSE 0 END) as fixed,
+       sum(CASE WHEN review.resultEngine is not null and review.resultId = 9 THEN 1 ELSE 0 END) as fixedResultEngine
+       
+       from
+         asset a
+         left join stig_asset_map sa using (assetId)
+         left join current_group_rule cgr using (benchmarkId)
+         left join rule r using (ruleId)
+         left join review on (r.ruleId=review.ruleId and review.assetId=sa.assetId)
     group by
       sa.assetId,
       sa.benchmarkId
-      ) as source on sam.assetId = source.assetId and source.benchmarkId = sam.benchmarkId
-
-      set sam.minTs = source.minTs,
-          sam.maxTs = source.maxTs,
-
-          sam.saved = source.saved,
-          sam.savedResultEngine = source.savedResultEngine,
-          sam.submitted = source.submitted,
-          sam.submittedResultEngine = source.submittedResultEngine,
-          sam.rejected = source.rejected,
-          sam.rejectedResultEngine = source.rejectedResultEngine,
-          sam.accepted = source.accepted,
-          sam.acceptedResultEngine = source.acceptedResultEngine,
-
-          sam.highCount = source.highCount,
-          sam.mediumCount = source.mediumCount,
-          sam.lowCount = source.lowCount,
-          
-          sam.notchecked = source.notchecked,
-          sam.notcheckedResultEngine = source.notcheckedResultEngine,
-          sam.notapplicable = source.notapplicable,
-          sam.notapplicableResultEngine = source.notapplicableResultEngine,
-          sam.pass = source.pass,
-          sam.passResultEngine = source.passResultEngine,
-          sam.fail = source.fail,
-          sam.failResultEngine = source.failResultEngine,
-          sam.unknown = source.unknown,
-          sam.unknownResultEngine = source.unknownResultEngine,
-          sam.error = source.error,
-          sam.errorResultEngine = source.errorResultEngine,
-          sam.notselected = source.notselected,
-          sam.notselectedResultEngine = source.notselectedResultEngine,
-          sam.informational = source.informational,
-          sam.informationalResultEngine = source.informationalResultEngine,
-          sam.fixed = source.fixed,
-          sam.fixedResultEngine = source.fixedResultEngine      
+      )
+  update stig_asset_map sam
+    inner join source on sam.assetId = source.assetId and source.benchmarkId = sam.benchmarkId
+    set sam.minTs = source.minTs,
+        sam.maxTs = source.maxTs,
+        sam.saved = source.saved,
+        sam.savedResultEngine = source.savedResultEngine,
+        sam.submitted = source.submitted,
+        sam.submittedResultEngine = source.submittedResultEngine,
+        sam.rejected = source.rejected,
+        sam.rejectedResultEngine = source.rejectedResultEngine,
+        sam.accepted = source.accepted,
+        sam.acceptedResultEngine = source.acceptedResultEngine,
+        sam.highCount = source.highCount,
+        sam.mediumCount = source.mediumCount,
+        sam.lowCount = source.lowCount,
+        sam.notchecked = source.notchecked,
+        sam.notcheckedResultEngine = source.notcheckedResultEngine,
+        sam.notapplicable = source.notapplicable,
+        sam.notapplicableResultEngine = source.notapplicableResultEngine,
+        sam.pass = source.pass,
+        sam.passResultEngine = source.passResultEngine,
+        sam.fail = source.fail,
+        sam.failResultEngine = source.failResultEngine,
+        sam.unknown = source.unknown,
+        sam.unknownResultEngine = source.unknownResultEngine,
+        sam.error = source.error,
+        sam.errorResultEngine = source.errorResultEngine,
+        sam.notselected = source.notselected,
+        sam.notselectedResultEngine = source.notselectedResultEngine,
+        sam.informational = source.informational,
+        sam.informationalResultEngine = source.informationalResultEngine,
+        sam.fixed = source.fixed,
+        sam.fixedResultEngine = source.fixedResultEngine        
     `
-
 
 ]
 
@@ -129,7 +125,7 @@ const downMigration = [
   `
   ALTER TABLE stig_asset_map 
   RENAME COLUMN saved TO savedManual,
-  RENAME COLUMN savedResultEngine TO saved,
+  RENAME COLUMN savedResultEngine TO savedAuto,
   RENAME COLUMN submitted TO submittedManual,
   RENAME COLUMN submittedResultEngine TO submittedAuto, 
   RENAME COLUMN rejected to rejectedManual,  
