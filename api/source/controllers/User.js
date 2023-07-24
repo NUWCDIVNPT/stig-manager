8df70ac4b52de63d9b36beb3d2ea631b5d9386bc
csmig <csmig@csmig.com>
2023-07-24 15:30:10 -0400

    feat: delete handling

--- api/source/controllers/User.js
+++ api/source/controllers/User.js
@@ -52,7 +52,7 @@ module.exports.deleteUser = async function deleteUser (req, res, next) {
       let userId = req.params.userId
       let projection = req.query.projection
       let userData = await User.getUserByUserId(userId, ['statistics'], elevate, req.userObject)
-      if (userData.statistics.lastAccess) {
+      if (userData?.statistics?.lastAccess) {
         // User has accessed the system, so we need to reject the request
         throw new SmError.UnprocessableError('User has accessed the system. Use PATCH to remove collection grants or configure Authentication provider to reject user entirely.')
       }
