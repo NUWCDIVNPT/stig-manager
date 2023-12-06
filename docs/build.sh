66fcba68d5d3eefac8661d55458a0d9b8ee45a33
cd-rite <61710958+cd-rite@users.noreply.github.com>
2023-12-06 16:50:01 -0500

    docs: build updates  (#1162)
    * chore: update docs dependencies
    
    * wip
    
    * tweaked build materials a bit
    
    * added versions
    
    * updates to docs build process

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SPHINX_IMAGE=sphinxdoc/sphinx:5.3.0
+SPHINX_IMAGE_W_REQUIREMENTS=sphinx-w-requirements
 
 # Change to this script directory
 cd "$(dirname "$(realpath "$0")")"
@@ -8,5 +8,6 @@ cd "$(dirname "$(realpath "$0")")"
 find _build -type f -not -name '.gitignore' -delete
 find _build -type d -empty -delete
 
-docker run --rm -v $(pwd):/docs $SPHINX_IMAGE \
-  /bin/bash -c "pip3 install -r /docs/requirements.txt && make html && chown -R $(id -u):$(id -g) /docs/_build"
+docker build -t $SPHINX_IMAGE_W_REQUIREMENTS .
+
+docker run --rm -v $(pwd):/docs $SPHINX_IMAGE_W_REQUIREMENTS
