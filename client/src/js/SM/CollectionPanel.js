d5db9f14e78ee712b6ec481e3fcd86c7280ffb71
csmig <33138761+csmig@users.noreply.github.com>
2024-07-24 14:36:00 +0000

    refactor: remove web app request for labels projection (#1328)

--- client/src/js/SM/CollectionPanel.js
+++ client/src/js/SM/CollectionPanel.js
@@ -1675,6 +1675,8 @@ SM.CollectionPanel.showCollectionTab = async function (options) {
       return
     }
 
+    SM.Cache.updateCollectionLabels(collectionId)
+
     const gState = {}
 
     gState.labelIds = initialLabelIds
@@ -1819,7 +1821,7 @@ SM.CollectionPanel.showCollectionTab = async function (options) {
       ],
       listeners: {
         tabchange: function (tp) {
-          updateData({event: 'tabchange'})
+          if (!tp.firstShow) updateData({ event: 'tabchange' })
           tp.firstShow = false
         }
       }
