bf1f9d10e9a5f981e7a3538d08c85f415167f5bd
cd-rite <github-rite@rite-solutions.com>
2025-07-31 13:11:40 -0400

    ran update launchers

--- api/launchers/stig-manager.sh
+++ api/launchers/stig-manager.sh
@@ -110,17 +110,17 @@
 # export STIGMAN_CLIENT_ID=
 
 #==============================================================================
-# STIGMAN_CLIENT_IDLE_TIMEOUT
+# STIGMAN_CLIENT_ADMIN_TIMEOUT
 #
-#  | Default: "15" | The maximum time (in minutes) a user can be inactive in the
-#  web client before expiring their access token and requiring reauthorization.
-#  Activity is defined as mouse click, keypress, or scrolling in any tab or
-#  window of a same-origin browsing context group. Set to zero to disable idle
-#  detection.
+#  | Default: "10" | The maximum time (in minutes) a user with admin privileges
+#  can be inactive in the web client before expiring their access token and
+#  requiring reauthorization. Activity is defined as mouse click, keypress, or
+#  scrolling in any tab or window of a same-origin browsing context group. Set
+#  to zero to disable idle detection.
 #
 #  Affects: Client
 #==============================================================================
-# export STIGMAN_CLIENT_IDLE_TIMEOUT=
+# export STIGMAN_CLIENT_ADMIN_TIMEOUT=
 
 #==============================================================================
 # STIGMAN_CLIENT_OIDC_PROVIDER
@@ -181,6 +181,19 @@
 #  Affects: Client
 #==============================================================================
 # export STIGMAN_CLIENT_STRICT_PKCE=
+
+#==============================================================================
+# STIGMAN_CLIENT_USER_TIMEOUT
+#
+#  | Default: "15" | The maximum time (in minutes) a regular user can be
+#  inactive in the web client before expiring their access token and requiring
+#  reauthorization. Activity is defined as mouse click, keypress, or scrolling
+#  in any tab or window of a same-origin browsing context group. Set to zero to
+#  disable idle detection.
+#
+#  Affects: Client
+#==============================================================================
+# export STIGMAN_CLIENT_USER_TIMEOUT=
 
 #==============================================================================
 # STIGMAN_CLIENT_WELCOME_IMAGE 
