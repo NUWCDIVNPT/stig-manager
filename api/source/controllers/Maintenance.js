c26e029d76546c235221c6653e5b1370f12cf342
csmig <carlsmigielski@gmail.com>
2025-08-28 12:00:50 -0400

    remove unused maintenance controller

--- api/source/controllers/Maintenance.js
+++
@@ -1,9 +0,0 @@
-module.exports.getMaintenancePathAvailable = async function (req, res, next) {
-  try {
-    if (!req.query.elevate) throw new SmError.PrivilegeError()
-    res.status(204).end()
-  }
-  catch (err) {
-    next(err)
-  }
-}
