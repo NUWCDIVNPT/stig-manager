ae1d0f1407e577e23f15980f19b206b3b2de46a7
Matte22 <mferreira1264@gmail.com>
2026-01-28 13:05:07 -0500

    remove old tablist component feature

--- clientV2/src/features/TabList/components/TabList.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import TabListContent from './TabListContent.vue'
-</script>
-
-<template>
-  <router-view v-slot="{ Component, route }">
-    <TabListContent :current-component="Component" :current-route="route" />
-  </router-view>
-</template>
